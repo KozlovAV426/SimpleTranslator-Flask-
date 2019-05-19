a73055c934fa8343580fcee7f7fb53ea5c0ff8a8
anatoly <example@gmail.com>
2019-05-19 21:58:31 +0300

    second iteration

--- app.py
+++ app.py
@@ -5,55 +5,50 @@ from work_with_database import get_rows_from_database, add_to_database, create_d
 from py_translator import Translator
 
 from flask import Flask, render_template, redirect, request
-from config import Configuration, INPUT, TRANSLATION, LANGUAGE, LENGTH
+from config import Configuration, INPUT, TRANSLATION, LANGUAGE, LENGTH, MAX_LAST_REQUEST_COUNTER 
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
-
-translation = ''
-messages = []
 
 create_database()
 
 
 @app.route('/', methods=['GET'])
 def index():
-	messages.clear()
-	translation = ''
 	return render_template('index.html')
+
+
+def translate_redirector(text=' ', language='en', show_previous=False):
+	if len(text) == 0:
+		return render_template('translator_wrong.html')
+
+	translation = Translator().translate(text=text, dest=language).text
+	messages = []
+
+	if (show_previous):
+		add_to_database(text, translation, language, len(text))
+		rows = get_rows_from_database()
+		for i in range(len(rows) - 1, -1, -1):
+			if (rows[i][LANGUAGE] == language):
+				messages.append(rows[i])
+				if (len(messages) == MAX_LAST_REQUEST_COUNTER):
+					break
+
+	return render_template('translator.html', messages=messages, content=translation)
+
 
 
 @app.route('/translator', methods=['GET'])
 def translator():
-	global translation
-	return render_template('translator.html', messages=messages, content=translation)
+	return translate_redirector()
 
 
-@app.route('/translate', methods=['POST'])
+@app.route('/translation_query', methods=['POST'])
 def translate():
 
-	text = request.form['text']
-	language = request.form['lang']
-
-	if len(text) == 0:
-		return render_template('translator_wrong.html')
-
-	global translation
-	translation = Translator().translate(text=text, dest=language).text
-
-	global messages
-	messages.clear()
-
-	add_to_database(text, translation, language, len(text))
-
-	rows = get_rows_from_database()
-
-	for i in range(len(rows) - 1, -1, -1):
-		if (rows[i][LANGUAGE] == language):
-			messages.append(rows[i])
-			if (len(messages) == 5):
-				break
-	return redirect('translator')
+	text = request.form.get('text')
+	language = request.form.get('lang')
+	return translate_redirector(text, language, True)
 
 
 @app.route('/database')
@@ -65,13 +60,13 @@ def database():
 @app.route('/statistics')
 def statistics():
 	rows = get_rows_from_database()
-	statistics = dict()
+	statistics = {}
 	if len(rows) > 0:
 		minlength = rows[0][LENGTH]
 		minlength_request = rows[0]
 		for row in rows:
 			if row[LENGTH] < minlength:
-				minlength = row[3]
+				minlength = row[LENGTH]
 				minlength_request = row
 			if row[LANGUAGE] in statistics.keys():
 				statistics[row[LANGUAGE]] += 1
